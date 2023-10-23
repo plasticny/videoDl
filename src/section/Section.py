ee8d22bc83ba6fe3a396efad12a668810a00ff2e
Peter, Cheung <2220354896@qq.com>
2023-10-23 09:02:54 +0800

    [section.py] add do show header or footer

--- src/section/Section.py
+++ src/section/Section.py
@@ -3,6 +3,8 @@ import sys
 class Section:
   def __init__(self, title):
     self.title = title
+    self.doShowHeader = True
+    self.doShowFooter = True
   
   @property
   def header (self):
@@ -11,16 +13,18 @@ class Section:
   @property
   def footer (self):
     return ''
-  
+    
   def run (self, bodyFunc=None):
     sys.stdout.flush()
         
     result = None
     
-    print(self.header)
+    if self.doShowHeader:
+      print(self.header)
     if callable(bodyFunc):
       result = bodyFunc()
-    print(self.footer)
+    if self.doShowFooter:
+      print(self.footer)
     
     return result
   
