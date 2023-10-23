da8affab675b7632fbadf616a963d8049e66fb04
Peter, Cheung <2220354896@qq.com>
2023-10-24 06:27:25 +0800

    [Section] add header type subheader

--- src/section/Section.py
+++ src/section/Section.py
@@ -1,14 +1,25 @@
 import sys
+from enum import Enum
+
+class HeaderType(Enum):
+  HEADER = 1
+  SUB_HEADER = 2
 
 class Section:
   def __init__(self, title):
     self.title = title
     self.doShowHeader = True
     self.doShowFooter = True
+
+    self.headerType = HeaderType.HEADER
   
   @property
   def header (self):
     return f"================= {self.title} ================="
+  
+  @property
+  def subHeader (self):
+    return f"## {self.title}"
   
   @property
   def footer (self):
@@ -19,10 +30,18 @@ class Section:
         
     result = None
     
+    # header
     if self.doShowHeader:
-      print(self.header)
+      if self.headerType == HeaderType.HEADER:
+        print(self.header)
+      elif self.headerType == HeaderType.SUB_HEADER:
+        print(self.subHeader)
+
+    # body
     if callable(bodyFunc):
       result = bodyFunc()
+
+    # footer
     if self.doShowFooter:
       print(self.footer)
     
