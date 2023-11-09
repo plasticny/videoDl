3e38443df266ce4274db6ccfe159096fe3d94d37
Peter, Cheung <2220354896@qq.com>
2023-11-09 08:16:04 +0800

    run ffmpeg with package
    run ffmpeg with ffmpeg-python package

--- src/service/merger.py
+++ src/service/merger.py
@@ -1,34 +1,48 @@
 from enum import Enum
 
-from service.commandUtils import runCommand, FFMPEG_EXEC
+import ffmpeg
 
 class Message (Enum):
+  NO_INPUT_FILE = 'Must provide video or audio file path'
   MERGE_FAILED = 'Merge failed'
 
-def merge (videoPath:str, audioPath:str, outputDir:str, videoFormat='copy', quiet=False) :
-  result = runCommand(
-    execCommand=FFMPEG_EXEC, 
-    paramCommands=[
-      '-i',
-        videoPath,
-        
-      '-i',
-        audioPath,
-        
-      '-c:v',
-        videoFormat,
-        
-      '-c:a',
-        'aac',
-        
-      '-c:s',
-        'mov_text',
-        
-      outputDir,
+def mergeVAS (
+    outputPath:str,
+    videoPath:str=None, audioPath:str=None, subtitlePath:str=None,
+    quiet=False
+  ):
+  """
+    Merge video, audio and subtitle into one file. For lyd.
 
-      '-hide_banner -loglevel error' if quiet else ''
-    ]
+    Args:
+      the corresponding components (video, audio, subtitle) will be extracted
+      from the file in `videoPath`, `audioPath` and `subtitlePath`\n
+
+      `videoPath` and `audioPath` can be None, but not both
+  """
+  if videoPath is None and audioPath is None:
+    raise Exception(Message.NO_INPUT_FILE.value)
+
+  # construct input streams and filename
+  stream_n_nm = []
+  if videoPath is not None:
+    stream_n_nm.append(ffmpeg.input(videoPath)['v'])
+  if audioPath is not None:
+    stream_n_nm.append(ffmpeg.input(audioPath)['a'])
+  if subtitlePath is not None:
+    stream_n_nm.append(ffmpeg.input(subtitlePath)['s'])
+  stream_n_nm.append(outputPath)
+
+  # run ffmpeg
+  ff : ffmpeg = ffmpeg.output(
+    *stream_n_nm,
+    vcodec='libx264', 
+    acodec='aac', 
+    scodec='mov_text',
+    loglevel='quiet' if quiet else 'info'
   )
-
-  if result.returncode != 0:
+  
+  try:
+    ff.run(cmd='ffmpeg\\ffmpeg', overwrite_output=True, capture_stderr=True)
+  except ffmpeg.Error:
     raise Exception(Message.MERGE_FAILED.value)
