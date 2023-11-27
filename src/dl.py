96c3e0b611bd48812e9417f6621e985fc35d39bb
Peter, Cheung <2220354896@qq.com>
2023-11-27 17:03:50 +0800

    debug dl

--- src/dl.py
+++ src/dl.py
@@ -1,6 +1,8 @@
 import service.packageChecker as packageChecker
 packageChecker.check()
 
+from colorama import Fore, Style
+
 from section.Section import Section, HeaderType
 from section.UrlSection import UrlSection
 from section.LoginSection import LoginSection
@@ -29,7 +31,11 @@ class Dl:
 
       print('Getting download informaton...', end='\n\n')
       md = fetchMetaData(url, opts)
-    
+
+      if md.isPlaylist():
+        print(f'{Fore.YELLOW}Playlist is not supported in dl currently.{Style.RESET_ALL}')
+        continue
+
       # list format
       ListFormatSection(title='List Format').run(url, opts)
       
@@ -45,27 +51,27 @@ class Dl:
   
   def setup(self, md:MetaData, opts:Opts) -> Opts:
     # subtitle
-    opts = SubTitleSection(
+    opts_ls = SubTitleSection(
       title='Subtitle',
       doShowFooter=False,
       headerType=HeaderType.SUB_HEADER
-    ).run(md, opts)
+    ).run(md, opts_ls=[opts])
 
     # format
     opts = FormatSection(
       title='Format',
       doShowFooter=False,
       headerType=HeaderType.SUB_HEADER
-    ).run(opts)
+    ).run(opts_ls[0])
 
     # output dir
-    opts = OutputSection(
+    opts_ls = OutputSection(
       title='Output',
       doShowFooter=False,
       headerType=HeaderType.SUB_HEADER
-    ).run(opts, askName=False)
+    ).run(opts_ls=[opts], askName=False)
 
-    return opts
+    return opts_ls[0]
 
 if __name__ == "__main__":
   Dl().run()
