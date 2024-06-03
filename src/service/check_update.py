c1f265921d84184085095519ac4a767610263adb
Peter, Cheung <2220354896@qq.com>
2024-06-03 17:59:48 +0800

    not check update in production env

--- src/service/check_update.py
+++ src/service/check_update.py
@@ -2,8 +2,12 @@ from requests import get
 from importlib.metadata import version
 from pip import main as pip
 from colorama import Fore, Style
+from os import environ
 
 def check_update():
+  if 'RUNTIME_ENV' in environ and environ['RUNTIME_ENV'] == 'prod':
+    return
+  
   print(f'{Fore.CYAN}Checking for updates...{Style.RESET_ALL}')
   releases : dict = get('https://pypi.org/pypi/yt_dlp/json').json()['releases']
   versions = list(releases.keys())
