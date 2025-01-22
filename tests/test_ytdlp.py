ad34ffc8a33930d3289112f597698971c919bf92
Peter, Cheung <2220354896@qq.com>
2025-01-21 20:40:16 -0600

    add testing for prepare ytdlp

--- tests/test_ytdlp.py
+++ tests/test_ytdlp.py
@@ -1,7 +1,10 @@
 from unittest.mock import patch, Mock
 from uuid import uuid4
+from dataclasses import dataclass
+from typing import Optional
+from pytest import raises as pytest_raises
 
-from src.service.ytdlp import Ytdlp, YT_DLP_PATH as EXE_NM
+from src.service.ytdlp import Ytdlp, YT_DLP_PATH as EXE_NM, YT_DLP_URL
 
 def test_build_base_cmd ():
   # empty opt
@@ -58,7 +61,7 @@ def test_build_base_cmd ():
   assert f'--sub-lang {",".join(opt["subtitleslangs"])}' in ytdlp.base_cmd
   assert f'-P {opt["paths"]["home"]}' in ytdlp.base_cmd
   assert f'-o {opt["outtmpl"]}' in ytdlp.base_cmd
-    
+
 @patch('src.service.ytdlp.loads')
 @patch('src.service.ytdlp.popen')
 @patch('src.service.ytdlp.Ytdlp._build_base_cmd')
@@ -103,3 +106,63 @@ def test_download (base_cmd_mock: Mock, run_cmd_mock: Mock):
   assert len(cmd_comp) == 2
   assert base_cmd in cmd_comp
   assert url in cmd_comp
+
+@patch('builtins.open')
+@patch('src.service.ytdlp.get')
+@patch('src.service.ytdlp.exists')
+def test_ensure_installed (
+  exists_mock: Mock,
+  get_request_mock: Mock,
+  open_mock: Mock
+):
+  class fakeResponse:
+    def __init__(self, status_code: int):
+      self.status_code = status_code
+      self.content = str(uuid4())
+
+  def fake_open (path: str, mode: str):
+    class fakeBufferedWriter:
+      def __enter__ (self):
+        return self
+      
+      def __exit__ (self, *args):
+        pass
+
+      def write (self, content: str):
+        pass
+    return fakeBufferedWriter()
+
+  @dataclass
+  class Case:
+    exists_ret: bool
+    get_request_ret: Optional[fakeResponse]
+    error_raised: bool
+
+  case_ls: list[Case] = [
+    # already installed
+    Case(True, None, False),
+    Case(False, fakeResponse(200), False),
+    Case(False, fakeResponse(404), True)
+  ]
+
+  for case in case_ls:
+    exists_mock.reset_mock()
+    get_request_mock.reset_mock()
+    open_mock.reset_mock()
+
+    exists_mock.return_value = case.exists_ret
+    get_request_mock.return_value = case.get_request_ret
+    open_mock.side_effect = fake_open
+
+    if case.error_raised:
+      with pytest_raises(Exception):
+        Ytdlp().ensure_installed()
+    else:    
+      Ytdlp().ensure_installed()
+
+    if not case.exists_ret:
+      get_request_mock.assert_called_once()
+      get_request_mock.call_args[0][0] == YT_DLP_URL
+
+      if not case.error_raised:
+        open_mock.assert_called_once_with(EXE_NM, 'wb')
